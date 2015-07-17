aab9fa60b94cf605f732a61302beeea6cf14070a
Jonas Eklöf <jonas@polymorphic.se>
2015-07-17 17:28:51 +0000

    added status URL

--- rivendells.py
+++ rivendells.py
@@ -1,11 +1,18 @@
-import os
-from flask import Flask, redirect
+import os,json,traceback
+from flask import Flask, redirect, Response
 
 app = Flask(__name__)
 
 @app.route('/')
 def redirect_to_blog():
     return redirect('http://rivendellsbloggen.blogspot.com', code=302)
+    
+@app.route('/status')
+def status():
+    try:
+        return Response(json.dumps({'status': 'UP'}), mimetype='application/json')
+    except:
+        traceback.print_exc()
 
 if __name__ == '__main__':
     port = int(os.environ.get('PORT', 5000))
